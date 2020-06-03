b79e95b7a4bb2cd1739838c56fb252d665705d9a
Ingrown29 <anonym6933@gmail.com>
2020-06-03 17:14:25 +0200

    Hotovy postup prac

--- src/main/java/sk/fri/uniza/model/DataDouble.java
+++ src/main/java/sk/fri/uniza/model/DataDouble.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
+@Entity
+@JsonTypeName("double")
 public class DataDouble extends AbstractData<Double> {
 
-
+    @Column(name = "double_value")
     private Double value;
 
     @Override
--- src/main/java/sk/fri/uniza/model/DataInteger.java
+++ src/main/java/sk/fri/uniza/model/DataInteger.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
+@Entity
+@JsonTypeName("integer")
 public class DataInteger extends AbstractData<Integer> {
 
+    @Column(name = "integer_value")
     private Integer Value;
 
     @Override
